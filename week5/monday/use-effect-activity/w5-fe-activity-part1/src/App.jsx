8936d694e0cc9dae188f26e9f8a1377e6bc6bcc1
Tanvir Rahman Nibir <TANVIR.NIBIR@metropolia.fi>
2025-09-15 13:31:20 +0300

    Monday_use_affect

---
+++ week5/monday/use-effect-activity/w5-fe-activity-part1/src/App.jsx
@@ -0,0 +1,15 @@
+import { useState } from 'react'
+import UseEffectTest from './UseEffectTest'
+import './App.css'
+
+function App() {
+
+
+  return (
+    <>
+      <UseEffectTest />    
+    </>
+  )
+}
+
+export default App
